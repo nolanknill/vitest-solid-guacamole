34a3aadf2319972fa1b0da97243e5d1da491ac17
Nolan Knill <nolanknill@gmail.com>
2024-07-09 13:15:52 -0400

    Adds tests for BankAccount  withdrawals

--- BankAccount.js
+++ BankAccount.js
@@ -1,10 +1,24 @@
 export default class BankAccount {
-    constructor(pin) {
+    constructor(pin, notifyBank) {
         this.pin = pin;
         this.balance = 0;
+        this.notifyBank = notifyBank; 
     }
 
     deposit(amount) {
         this.balance += amount;
+    }
+    
+    withdraw(amount, pin) {
+        if (this.balance < amount) {
+            this.notifyBank("Insufficient Balance requested");
+            throw new Error("Insufficient Balance");
+        }
+
+        if (pin !== this.pin) {
+            throw new Error("Invalid pin");
+        }
+
+        this.balance -= amount;
     }
 }
--- BankAccount.test.js
+++ BankAccount.test.js
@@ -1,19 +1,16 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import BankAccount from "./BankAccount";
 
 describe("BankAccount", () => {
     it("is created with correct details", () =>{
-        /* 1. Arrange */
-        /* 2. Act */
         const account = new BankAccount("0123");
     
-        /* 3. Assert */
         expect(account).toBeInstanceOf(BankAccount);
         expect(account.pin).toEqual("0123");
         expect(account.balance).toEqual(0);
     })
 
-    it("can have money deposited", () => {
+    it("deposits money", () => {
         /* 1. Arrange */
         const account = new BankAccount();
     
@@ -23,4 +20,54 @@ describe("BankAccount", () => {
         /* 3. Assert */
         expect(account.balance).toEqual(1000000);
     });
+
+    it("withdraws money", () => {
+        /* 1. Arrange */
+        const account = new BankAccount("1234");
+    
+        /* 2. Act */
+        account.deposit(1000);
+        account.withdraw(500, "1234");
+    
+        /* 3. Assert */
+        expect(account.balance).toEqual(500);
+    })
+
+    it("doesn't withdraw more money than available", () => {
+        /* 1. Arrange */
+        const account = new BankAccount("1234");
+    
+        /* 2. Act */
+        account.deposit(1000);
+        expect(() => account.withdraw(2500, "1234")).toThrowError();
+    
+        /* 3. Assert */
+        expect(account.balance).toEqual(1000);
+    })
+
+    it("doesn't withdraw money with incorrect pin", () => {
+        /* 1. Arrange */
+        const account = new BankAccount("1234");
+    
+        /* 2. Act */
+        account.deposit(1000);
+        expect(() => account.withdraw(500)).toThrowError();
+    
+        /* 3. Assert */
+        expect(account.balance).toEqual(1000);
+    })
+
+    it("notifies the bank when insufficient balance is attempted to be withdrawn", () => {
+        /* 1. Arrange */
+        const mockNotifyFn = vi.fn();
+        const account = new BankAccount("1234", mockNotifyFn);
+    
+        /* 2. Act */
+        account.deposit(1000);
+        expect(() => account.withdraw(2500, "1234")).toThrowError();
+    
+        /* 3. Assert */
+        expect(mockNotifyFn).toBeCalled();
+        expect(account.balance).toEqual(1000);
+    })
 })
